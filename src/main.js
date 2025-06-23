d5e8c7287ce02ac16b22847c1f73b1294290caa8
Gabriel Williamson <gabrielwilliamson92@gmail.com>
2025-06-23 11:38:15 -0600

    added icons and toogle classes

--- src/main.js
+++ src/main.js
@@ -9,44 +9,78 @@ import {
   SourceEditing,
   Plugin,
   ButtonView,
+  IconObjectInlineLeft,
+  IconObjectInlineRight,
+  IconObjectCenter,
+  IconObjectFullWidth,
 } from "ckeditor5";
-
+import { initialData } from "./initial-data";
 import "ckeditor5/ckeditor5.css";
 
 export default class IframeAligner extends Plugin {
   init() {
     const editor = this.editor;
 
-    this._isRawHtmlSelected = false;
-
-    editor.model.document.selection.on("change:range", () => {
-      const selectedElement =
-        editor.model.document.selection.getSelectedElement();
-
-      this._isRawHtmlSelected =
-        selectedElement?.is("element", "rawHtml") ?? false;
-    });
-
     const alignments = [
-      { name: "alignLeft", label: "Left", class: "align-left" },
-      { name: "alignCenter", label: "Center", class: "align-center" },
-      { name: "alignRight", label: "Right", class: "align-right" },
+      {
+        name: "alignLeft",
+        label: "Align Html Left",
+        class: "align-left",
+        icon: IconObjectInlineLeft,
+      },
+      {
+        name: "alignCenter",
+        label: "Align Html Center",
+        class: "align-center",
+        icon: IconObjectCenter,
+      },
+      {
+        name: "alignRight",
+        label: "Align Html Right",
+        class: "align-right",
+        icon: IconObjectInlineRight,
+      },
+      {
+        name: "fullWidth",
+        label: "Full Width",
+        class: "embed-container-full",
+        icon: IconObjectFullWidth,
+      },
     ];
 
-    for (const { name, label, class: className } of alignments) {
+    alignments.forEach(({ name, label, class: className, icon }) => {
       editor.ui.componentFactory.add(name, (locale) => {
         const button = new ButtonView(locale);
 
         button.set({
           label,
-          withText: true,
+          icon,
+          withText: false,
           tooltip: true,
+          isEnabled: false,
+          isOn: false,
         });
 
         button.on("execute", () => this.setAlignment(className));
+
+        editor.model.document.selection.on("change:range", () => {
+          const selectedElement =
+            editor.model.document.selection.getSelectedElement();
+          const isRawHtml = selectedElement?.is("element", "rawHtml") ?? false;
+
+          button.isEnabled = isRawHtml;
+
+          if (isRawHtml) {
+            const value = selectedElement.getAttribute("value") || "";
+            button.isOn = value.includes(className);
+          } else {
+            button.isOn = false;
+          }
+        });
+
         return button;
       });
-    }
+    });
   }
 
   setAlignment(className) {
@@ -54,28 +88,39 @@ export default class IframeAligner extends Plugin {
     const modelElement = editor.model.document.selection.getSelectedElement();
 
     if (!modelElement || !modelElement.is("element", "rawHtml")) {
-      console.warn("No HtmlEmbed widget selected");
+      console.warn("No rawHtml widget selected");
       return;
     }
 
     const html = modelElement.getAttribute("value");
     if (!html) return;
 
-    let wrappedHtml;
-
-    const wrapRegex =
-      /^<div class="align-(left|center|right)">([\s\S]*)<\/div>$/;
+    const wrapRegex = /^<div class="([^"]+)">([\s\S]*)<\/div>$/;
     const match = html.trim().match(wrapRegex);
 
+    let classes = [];
+    let innerContent = html;
+
     if (match) {
-      const innerContent = match[2];
-      wrappedHtml = `<div class="${className}">${innerContent}</div>`;
+      classes = match[1].split(" ");
+      innerContent = match[2];
+    }
+
+    if (classes.includes(className)) {
+      classes = classes.filter((cls) => cls !== className);
     } else {
-      wrappedHtml = `<div class="${className}">${html}</div>`;
+      classes.push(className);
+    }
+
+    let newHtml;
+    if (classes.length > 0) {
+      newHtml = `<div class="${classes.join(" ")}">${innerContent}</div>`;
+    } else {
+      newHtml = innerContent;
     }
 
     editor.model.change((writer) => {
-      writer.setAttribute("value", wrappedHtml, modelElement);
+      writer.setAttribute("value", newHtml, modelElement);
     });
   }
 }
@@ -94,6 +139,7 @@ const editorConfig = {
       "alignLeft",
       "alignCenter",
       "alignRight",
+      "fullWidth",
     ],
     shouldNotGroupWhenFull: false,
   },
@@ -118,44 +164,19 @@ const editorConfig = {
     ],
   },
 
-  initialData: "",
+  initialData: initialData,
   licenseKey: LICENSE_KEY,
   placeholder: "Type or paste your content here!",
 };
 
 ClassicEditor.create(document.querySelector("#editor"), editorConfig).then(
   (editor) => {
-    editor.model.document.on("change:data", () => {
-      console.log("event");
+    const data = editor.getData();
+    document.querySelector("#preview-content").innerHTML = data;
+
+    editor.model.document.on("change", () => {
       const data = editor.getData();
       document.querySelector("#preview-content").innerHTML = data;
     });
   }
 );
-
-// snipets
-document.addEventListener("DOMContentLoaded", () => {
-  const youtube = document.getElementById("youtube");
-
-  youtube.addEventListener("click", () => {
-    navigator.clipboard
-      .writeText(
-        `<iframe width="560" height="315" src="https://www.youtube.com/embed/_CL6n0FJZpk?si=3XSzvNWY-Z9Cj9gd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
-      )
-      .then(() => {
-        youtube.innerText = "YoutTube Copied";
-      });
-  });
-
-  const twitter = document.getElementById("twitter");
-
-  twitter.addEventListener("click", () => {
-    navigator.clipboard
-      .writeText(
-        `<blockquote class="twitter-tweet"><p lang="da" dir="ltr">realDevModel <a href="https://t.co/foc8K8ltTa">https://t.co/foc8K8ltTa</a> <a href="https://t.co/XZT2Z77rJG">pic.twitter.com/XZT2Z77rJG</a></p>&mdash; Programmer Humor (@PR0GRAMMERHUM0R) <a href="https://twitter.com/PR0GRAMMERHUM0R/status/1936846674791469120?ref_src=twsrc%5Etfw">June 22, 2025</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`
-      )
-      .then(() => {
-        twitter.innerHTML = "Twitter Copied";
-      });
-  });
-});
