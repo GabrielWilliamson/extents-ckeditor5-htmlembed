305ee597cf61a22ec9e39bec5f1041207a89663e
Gabriel Williamson <gabrielwilliamson92@gmail.com>
2025-07-01 10:27:40 -0600

    SDSDSDD

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const btn = document.getElementById("btn");
     btn.addEventListener("click", () => {
+        alert("vamo a compartir acontinuación");
         if (navigator.share) {
             navigator.share({
                 title: document.title,
                 text: 'Mira esta Pagina bro',
                 url: window.location.href,
             }).catch((error) => {
-                console.error('Error compartiendo:', error);
+                alert('No se pudo compartir el enlace', JSON.stringify(error));
+
             });
         } else {
 
